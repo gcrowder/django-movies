81d99aaf1186df80407efb83b43e098a109b0312
gcrowder <gregory.j.crowder@gmail.com>
2016-05-04 16:07:16 -0400

    Update .gitignore, movies/models, movies/views and movieratings/urls.

--- movieratings/movies/models.py
+++ movieratings/movies/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Rater(models.Model):
+    rater_id = models.IntegerField()
+    ratings = models.ForeignKey(Ratings, on_delete=models.CASCADE)
+
+
+class Movies(models.Model):
+    movie_id = models.IntegerField()
+    title = models.CharField(max_length=200)
+    ratings = models.ForeignKey(Ratings, on_delete=models.CASCADE)
+
+
+class Ratings(models.Model):
+    rater_id = models.IntegerField()
+    movie_id = models.IntegerField()
+    rating = models.IntegerField()
