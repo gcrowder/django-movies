44ddce377a4190a3596629a2330e747c4978866c
gcrowder <gregory.j.crowder@gmail.com>
2016-05-04 20:44:12 -0400

    Changed models around.

--- movieratings/movies/admin.py
+++ movieratings/movies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Ratings, Rater, Movies
+
+admin.site.register(Ratings)
+admin.site.register(Rater)
+admin.site.register(Movies)
--- movieratings/movies/models.py
+++ movieratings/movies/models.py
@@ -1,18 +1,12 @@
 from django.db import models
 
 
-class Rater(models.Model):
+class Ratings(models.Model):
     rater_id = models.IntegerField()
-    ratings = models.ForeignKey(Ratings, on_delete=models.CASCADE)
+    movie_id = models.IntegerField()
+    rating = models.IntegerField()
 
 
 class Movies(models.Model):
     movie_id = models.IntegerField()
     title = models.CharField(max_length=200)
-    ratings = models.ForeignKey(Ratings, on_delete=models.CASCADE)
-
-
-class Ratings(models.Model):
-    rater_id = models.IntegerField()
-    movie_id = models.IntegerField()
-    rating = models.IntegerField()
